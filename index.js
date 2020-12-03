1f9137ed0a0f90885a57beee05a4792ff42f8ac6
Kevin Wilson <kevin.wilson1@caci.com>
2020-12-03 07:48:23 -0700

    feat(toastui): upgrade toast with better integration

--- index.js
+++ index.js
@@ -142,23 +142,14 @@ const sharedResources = [
     files: ['images']
   },
   {
-    source: resolver.resolveModulePath('codemirror/lib', __dirname),
-    target: 'vendor/tui-editor',
-    css: ['codemirror.css']
-  },
-  {
-    source: resolver.resolveModulePath('tui-editor/dist', __dirname),
-    target: 'vendor/tui-editor',
-    css: ['tui-editor.min.css']
+    source: resolver.resolveModulePath('@toast-ui/editor/dist', __dirname),
+    target: 'vendor/toastui',
+    css: ['toastui-editor.css'],
+    scripts: ['toastui-editor.js']
   },
   {
     source: resolver.resolveModulePath('markdown-it/dist', __dirname),
     scripts: ['markdown-it.min.js']
-  },
-  {
-    source: 'vendor/os-minified/',
-    target: 'vendor/os-minified/',
-    files: ['os-tui-editor.min.js']
   },
   {
     source: resolver.resolveModulePath('crossfilter2', __dirname),
--- vendor-min.js
+++ vendor-min.js
@@ -1,74 +1,78 @@
-#!/usr/bin/env node
+// #!/usr/bin/env node
 
-'use strict';
+// 'use strict';
 
-const path = require('path');
-const {compile} = require('opensphere-build-closure-helper');
-const {resolveModulePath} = require('opensphere-build-resolver/utils');
+// // I see... I see now. This is called in package.json by action compile:vendor-min, and included in all the apps.
+// // so we'd need to disable it so that _all_ apps can run.
+// // which means, long run, MRs to remove mention of vendor-min from all.
 
-
-/**
- * Path to the tui-editor module.
- * @type {string}
- */
-const tuiPath = resolveModulePath('tui-editor', __dirname);
+// const path = require('path');
+// const {compile} = require('opensphere-build-closure-helper');
+// const {resolveModulePath} = require('opensphere-build-resolver/utils');
 
 
-/**
- * Resources for tuiEditor
- * @type {Array<Object>}
- */
-const tuiEditorResources = [
-  {
-    source: resolveModulePath('to-mark/dist', __dirname),
-    scripts: ['to-mark.min.js']
-  },
-  {
-    source: resolveModulePath('highlight.js/lib', __dirname),
-    scripts: ['highlight.js']
-  },
-  {
-    source: resolveModulePath('squire-rte/build', __dirname),
-    scripts: ['squire.js']
-  },
-  {
-    source: resolveModulePath('codemirror/lib', __dirname),
-    scripts: ['codemirror.js']
-  },
-  {
-    // Resolve the dependency from tui-editor. This prevents resolving a different version hoisted from other deps.
-    source: resolveModulePath('tui-code-snippet/dist', tuiPath),
-    scripts: ['tui-code-snippet.min.js']
-  },
-  {
-    source: path.join(tuiPath, 'dist'),
-    scripts: [
-      'tui-editor-Editor.min.js',
-      'tui-editor-extTable.min.js'
-    ]
-  }
-];
+// /**
+//  * Path to the tui-editor module.
+//  * @type {string}
+//  */
+// const tuiPath = resolveModulePath('tui-editor', __dirname);
 
 
-/**
- * Minify a set of vendor scripts.
- * @param {Array<Object>} resources List of scripts to minify.
- * @param {string} output The output file.
- * @param {string=} opt_optimizationLevel The optimization level.
- */
-const vendorMinify = function(resources, output, opt_optimizationLevel = 'SIMPLE_OPTIMIZATIONS') {
-  // Assemble script paths to include in the compilation.
-  const fileList = [];
-  resources.forEach((lib) => {
-    fileList.push(...lib.scripts.map((script) => path.join(lib.source, script)));
-  });
+// /**
+//  * Resources for tuiEditor
+//  * @type {Array<Object>}
+//  */
+// const tuiEditorResources = [
+//   {
+//     source: resolveModulePath('to-mark/dist', __dirname),
+//     scripts: ['to-mark.min.js']
+//   },
+//   {
+//     source: resolveModulePath('highlight.js/lib', __dirname),
+//     scripts: ['highlight.js']
+//   },
+//   {
+//     source: resolveModulePath('squire-rte/build', __dirname),
+//     scripts: ['squire.js']
+//   },
+//   {
+//     source: resolveModulePath('codemirror/lib', __dirname),
+//     scripts: ['codemirror.js']
+//   },
+//   {
+//     // Resolve the dependency from tui-editor. This prevents resolving a different version hoisted from other deps.
+//     source: resolveModulePath('tui-code-snippet/dist', tuiPath),
+//     scripts: ['tui-code-snippet.min.js']
+//   },
+//   {
+//     source: path.join(tuiPath, 'dist'),
+//     scripts: [
+//       'tui-editor-Editor.min.js',
+//       'tui-editor-extTable.min.js'
+//     ]
+//   }
+// ];
 
-  compile({
-    'js': fileList,
-    'compilation_level': opt_optimizationLevel,
-    'hide_warnings_for': ['/node_modules/'],
-    'js_output_file': output
-  });
-};
 
-vendorMinify(tuiEditorResources, 'vendor/os-minified/os-tui-editor.min.js');
+// /**
+//  * Minify a set of vendor scripts.
+//  * @param {Array<Object>} resources List of scripts to minify.
+//  * @param {string} output The output file.
+//  * @param {string=} opt_optimizationLevel The optimization level.
+//  */
+// const vendorMinify = function(resources, output, opt_optimizationLevel = 'SIMPLE_OPTIMIZATIONS') {
+//   // Assemble script paths to include in the compilation.
+//   const fileList = [];
+//   resources.forEach((lib) => {
+//     fileList.push(...lib.scripts.map((script) => path.join(lib.source, script)));
+//   });
+
+//   compile({
+//     'js': fileList,
+//     'compilation_level': opt_optimizationLevel,
+//     'hide_warnings_for': ['/node_modules/'],
+//     'js_output_file': output
+//   });
+// };
+
+// vendorMinify(tuiEditorResources, 'vendor/os-minified/os-tui-editor.min.js');
