784fa5d4df56422f887b72b09df0427210f918c6
Connor Gibson <connor.gibson@caci.com>
2020-12-15 20:04:34 -0700

    refactor(es6): run jscodeshift on opensphere/src/os/action

--- src/os/action/eventtype.js
+++ src/os/action/eventtype.js
@@ -1,12 +1,13 @@
-goog.provide('os.action.EventType');
+goog.module('os.action.EventType');
+goog.module.declareLegacyNamespace();
 
-goog.require('goog.events.EventType');
+const EventType = goog.require('goog.events.EventType');
 
 
 /**
  * @enum {string}
  */
-os.action.EventType = {
+exports = {
 
   // layer
   IDENTIFY: 'identify',
@@ -23,7 +24,7 @@ os.action.EventType = {
   FEATURE_LIST: 'layer:featureList',
 
   // map
-  COPY: goog.events.EventType.COPY,
+  COPY: EventType.COPY,
   RESET_VIEW: 'resetView',
   RESET_ROTATION: 'resetRotation',
   TOGGLE_VIEW: 'toggleView',
